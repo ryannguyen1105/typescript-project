fd0f16eb4e8df7c5be39f59ef181a08db91da39f
RYAN NGUYEN <nguyenthanhtai112005@gmail.com>
2026-03-27 17:20:10 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import fs from 'node:fs';
+
+
+let program: string;
+program = 'Golang'
+
+console.log(program);
+
+function add(a:any , b:any) {
+    return (a + b);
+}
+
+console.log(add(1,2));
+

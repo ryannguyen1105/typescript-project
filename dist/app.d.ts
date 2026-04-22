c9003c1b142c898d48c76d6769d6024e09295c77
RYAN NGUYEN <nguyenthanhtai112005@gmail.com>
2026-04-23 00:47:12 +0700

    .

---
+++ dist/app.d.ts
@@ -0,0 +1,15 @@
+declare const hobbies: string[];
+declare const activeHobbies: string[];
+declare const person: {
+    firstName: string;
+    age: number;
+};
+declare const copiedPerson: {
+    firstName: string;
+    age: number;
+};
+declare const add: (...numbers: number[]) => number;
+declare const addedNumbers: number;
+declare const hobby1: string | undefined, hobby2: string | undefined, remainingHobbies: string[];
+declare const userName: string, age: number;
+//# sourceMappingURL=app.d.ts.map
